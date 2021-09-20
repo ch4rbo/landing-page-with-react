824bbc3d2f51e26d27a21c462b1f0f6299e7aa9d
ch4rbo <baueratctu@gmail.com>
2021-09-20 20:04:24 +0000

    tomando forma

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -8,51 +8,121 @@ const Home = () => {
 	return (
 		<div>
 			<nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-				<div className="container-fluid">
-					<a className="navbar-brand" href="#">
-						Start Bootstrap
-					</a>
-					<button
-						className="navbar-toggler"
-						type="button"
-						data-bs-toggle="collapse"
-						data-bs-target="#navbarSupportedContent"
-						aria-controls="navbarSupportedContent"
-						aria-expanded="false"
-						aria-label="Toggle navigation">
-						<span className="navbar-toggler-icon"></span>
-					</button>
-					<div
-						className="collapse navbar-collapse"
-						id="navbarSupportedContent">
-						<ul className="navbar-nav me-auto mb-2 mb-lg-0">
-							<li className="nav-item">
-								<a
-									className="nav-link active"
-									aria-current="page"
-									href="#">
-									Home
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									About
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Services
-								</a>
-							</li>
-							<li className="nav-item">
-								<a className="nav-link" href="#">
-									Contact
-								</a>
-							</li>
-						</ul>
+				<div className="container-fluid d-flex justify-content-between">
+					<div>
+						<a className="navbar-brand" href="#">
+							Start Bootstrap
+						</a>
+					</div>
+					<div>
+						<div
+							className="collapse navbar-collapse"
+							id="navbarSupportedContent">
+							<ul className="navbar-nav me-auto mb-2 mb-lg-0">
+								<li className="nav-item">
+									<a
+										className="nav-link active"
+										aria-current="page"
+										href="#">
+										Home
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										About
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										Services
+									</a>
+								</li>
+								<li className="nav-item">
+									<a className="nav-link" href="#">
+										Contact
+									</a>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</nav>
+			<div className="container">
+				<div className="jumbotron">
+					<h1 className="display-4">Hello, world!</h1>
+					<p className="lead">
+						This is a simple hero unit, a simple jumbotron-style
+						component for calling extra attention to featured
+						content or information.
+					</p>
+					<hr className="my-4" />
+					<p>
+						It uses utility classes for typography and spacing to
+						space content out within the larger container.
+					</p>
+					<a
+						className="btn btn-primary btn-lg"
+						href="#"
+						role="button">
+						Learn more
+					</a>
+				</div>
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+
+				<div className="card" style={{ width: "18rem" }}>
+					<img src="..." className="card-img-top" alt="..." />
+					<div className="card-body">
+						<h5 className="card-title">Card title</h5>
+						<p className="card-text">
+							Some quick example text to build on the card title
+							and make up the bulk of the cards content.
+						</p>
+						<a href="#" className="btn btn-primary">
+							Go somewhere
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
